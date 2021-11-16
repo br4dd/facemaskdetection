d47493fd604b105612fc5627aef6e5d0b8950b8d
Marc Ryan Gil <gil.marcryan@gmail.com>
2021-11-17 02:54:45 +0800

    Initial Commit

---
+++ facemaskdetection/facemaskdetection/Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace facemaskdetection
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.topborder = new System.Windows.Forms.Panel();
+            this.bottomborder = new System.Windows.Forms.Panel();
+            this.poweredbytxt = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.launchbtn = new System.Windows.Forms.Button();
+            this.recordsbtn = new System.Windows.Forms.Button();
+            this.registerbtn = new System.Windows.Forms.Button();
+            this.logsbtn = new System.Windows.Forms.Button();
+            this.getstartedbtn = new System.Windows.Forms.Button();
+            this.taglinetxt = new System.Windows.Forms.Label();
+            this.bottomborder.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // topborder
+            // 
+            this.topborder.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(137)))), ((int)(((byte)(213)))), ((int)(((byte)(194)))));
+            this.topborder.Location = new System.Drawing.Point(0, 0);
+            this.topborder.Name = "topborder";
+            this.topborder.Size = new System.Drawing.Size(1112, 53);
+            this.topborder.TabIndex = 0;
+            // 
+            // bottomborder
+            // 
+            this.bottomborder.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(137)))), ((int)(((byte)(213)))), ((int)(((byte)(194)))));
+            this.bottomborder.Controls.Add(this.poweredbytxt);
+            this.bottomborder.Location = new System.Drawing.Point(0, 548);
+            this.bottomborder.Name = "bottomborder";
+            this.bottomborder.Size = new System.Drawing.Size(1112, 43);
+            this.bottomborder.TabIndex = 1;
+            // 
+            // poweredbytxt
+            // 
+            this.poweredbytxt.AutoSize = true;
+            this.poweredbytxt.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.poweredbytxt.Location = new System.Drawing.Point(455, 12);
+            this.poweredbytxt.Name = "poweredbytxt";
+            this.poweredbytxt.Size = new System.Drawing.Size(199, 16);
+            this.poweredbytxt.TabIndex = 0;
+            this.poweredbytxt.Text = "Powered by CloverByte, 2021";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(143)))), ((int)(((byte)(216)))), ((int)(((byte)(198)))));
+            this.panel1.Location = new System.Drawing.Point(497, 144);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(3, 324);
+            this.panel1.TabIndex = 2;
+            // 
+            // launchbtn
+            // 
+            this.launchbtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(27)))), ((int)(((byte)(185)))), ((int)(((byte)(146)))));
+            this.launchbtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.launchbtn.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.launchbtn.ForeColor = System.Drawing.Color.White;
+            this.launchbtn.Location = new System.Drawing.Point(563, 197);
+            this.launchbtn.Name = "launchbtn";
+            this.launchbtn.Size = new System.Drawing.Size(214, 38);
+            this.launchbtn.TabIndex = 3;
+            this.launchbtn.Text = "LAUNCH";
+            this.launchbtn.UseVisualStyleBackColor = false;
+            // 
+            // recordsbtn
+            // 
+            this.recordsbtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(27)))), ((int)(((byte)(185)))), ((int)(((byte)(146)))));
+            this.recordsbtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.recordsbtn.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.recordsbtn.ForeColor = System.Drawing.Color.White;
+            this.recordsbtn.Location = new System.Drawing.Point(828, 197);
+            this.recordsbtn.Name = "recordsbtn";
+            this.recordsbtn.Size = new System.Drawing.Size(214, 38);
+            this.recordsbtn.TabIndex = 4;
+            this.recordsbtn.Text = "RECORDS";
+            this.recordsbtn.UseVisualStyleBackColor = false;
+            // 
+            // registerbtn
+            // 
+            this.registerbtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(27)))), ((int)(((byte)(185)))), ((int)(((byte)(146)))));
+            this.registerbtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.registerbtn.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.registerbtn.ForeColor = System.Drawing.Color.White;
+            this.registerbtn.Location = new System.Drawing.Point(563, 269);
+            this.registerbtn.Name = "registerbtn";
+            this.registerbtn.Size = new System.Drawing.Size(214, 38);
+            this.registerbtn.TabIndex = 5;
+            this.registerbtn.Text = "REGISTER A FACE";
+            this.registerbtn.UseVisualStyleBackColor = false;
+            // 
+            // logsbtn
+            // 
+            this.logsbtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(27)))), ((int)(((byte)(185)))), ((int)(((byte)(146)))));
+            this.logsbtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.logsbtn.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.logsbtn.ForeColor = System.Drawing.Color.White;
+            this.logsbtn.Location = new System.Drawing.Point(828, 269);
+            this.logsbtn.Name = "logsbtn";
+            this.logsbtn.Size = new System.Drawing.Size(214, 38);
+            this.logsbtn.TabIndex = 6;
+            this.logsbtn.Text = "LOGS";
+            this.logsbtn.UseVisualStyleBackColor = false;
+            // 
+            // getstartedbtn
+            // 
+            this.getstartedbtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(113)))), ((int)(((byte)(93)))));
+            this.getstartedbtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.getstartedbtn.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.getstartedbtn.ForeColor = System.Drawing.Color.White;
+            this.getstartedbtn.Location = new System.Drawing.Point(563, 360);
+            this.getstartedbtn.Name = "getstartedbtn";
+            this.getstartedbtn.Size = new System.Drawing.Size(479, 38);
+            this.getstartedbtn.TabIndex = 7;
+            this.getstartedbtn.Text = "GET STARTED";
+            this.getstartedbtn.UseVisualStyleBackColor = false;
+            // 
+            // taglinetxt
+            // 
+            this.taglinetxt.AutoSize = true;
+            this.taglinetxt.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.taglinetxt.Location = new System.Drawing.Point(164, 348);
+            this.taglinetxt.Name = "taglinetxt";
+            this.taglinetxt.Size = new System.Drawing.Size(170, 50);
+            this.taglinetxt.TabIndex = 8;
+            this.taglinetxt.Text = "Mandate Safety, \r\nMandate Masks.";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(1111, 591);
+            this.ControlBox = false;
+            this.Controls.Add(this.taglinetxt);
+            this.Controls.Add(this.getstartedbtn);
+            this.Controls.Add(this.logsbtn);
+            this.Controls.Add(this.registerbtn);
+            this.Controls.Add(this.recordsbtn);
+            this.Controls.Add(this.launchbtn);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.bottomborder);
+            this.Controls.Add(this.topborder);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.bottomborder.ResumeLayout(false);
+            this.bottomborder.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel topborder;
+        private System.Windows.Forms.Panel bottomborder;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button launchbtn;
+        private System.Windows.Forms.Button recordsbtn;
+        private System.Windows.Forms.Button registerbtn;
+        private System.Windows.Forms.Button logsbtn;
+        private System.Windows.Forms.Button getstartedbtn;
+        private System.Windows.Forms.Label taglinetxt;
+        private System.Windows.Forms.Label poweredbytxt;
+    }
+}
+
